351e1e647323b8e7005912736471521e791974aa
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-17 15:07:51 +0530

    loops

---
+++ CPP_Crash/06_loops.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << "Abhishek" << endl;
+    }
+
+    int i = 1;
+    while (i <= 5)
+    {
+        cout << "ABhi" << endl;
+        i++;
+    }
+}
