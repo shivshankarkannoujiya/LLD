0e79cb19b0d6bbdfe2978f1122a71008d1d98171
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-01-17 14:31:43 +0530

    variabl and datatypes

---
+++ CPP_Crash/02_variablesDatatype.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // Declaration
+    // int age ;
+    // cout<<age<<endl; //Print garbage value
+
+    // Definition
+    int age = 20;
+
+    // manipulation or updation
+    age = 302;
+    cout << "My age is : " << age << endl;
+}
